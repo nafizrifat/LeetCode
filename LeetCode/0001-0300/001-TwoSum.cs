980f4a3bdc84eeaa3a8b0e8beea0d868b529384b
nafizrifat <imtiaz_rifat@yahoo.com>
2025-04-16 08:37:35 -0500

    1. Two Sum - Solution changed to using Dictionary

--- LeetCode/0001-0300/001-TwoSum.cs
+++ LeetCode/0001-0300/001-TwoSum.cs
@@ -1,6 +1,6 @@
 ﻿//-----------------------------------------------------------------------------
-// Runtime: 232ms
-// Memory Usage: 31.3 MB
+// Runtime: 1ms
+// Memory Usage: 48.96 MB
 // Link: https://leetcode.com/problems/two-sum/
 //-----------------------------------------------------------------------------
 
@@ -15,17 +15,50 @@ namespace LeetCode._0001_0300
 
         public int[] TwoSum(int[] nums, int target)
         {
-            int i, j;
-            for (i = 0; i < nums.Length; i++)
-                for (j = 1; j < nums.Length; j++)
+            ////Start: Solution using Two For Loop
+            ////Time Complexity: O(n^2) due to two for loops
+            //int i, j;
+            //for (i = 0; i < nums.Length; i++)
+            //    for (j = 1; j < nums.Length; j++)
+            //    {
+            //        if (nums[i] + nums[j] == target & i != j)
+            //        {
+            //            return new[] { i, j };
+            //        }
+            //    }
+
+            //return null;
+            ////End: Solution using Two For Loop
+            ///
+
+
+            //Start: Solution using Dictionary
+            //Time Complexity: O(n) due to one for loop
+
+            Dictionary<int, int> numDict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                //Calculate the complement needed to reach the target
+                int complement = target - nums[i];
+
+                //Check if the complement is already in the Dictionary
+                if (numDict.ContainsKey(complement))
                 {
-                    if (nums[i] + nums[j] == target & i != j)
-                    {
-                        return new[] { i, j };
-                    }
+
+                    //if found, return the indexes as an array
+                    return new int[] { numDict[complement], i };
                 }
 
-            return null;
+                //if not in the dictionaly then Add it
+                if (!numDict.ContainsKey(complement))
+                {
+                    numDict[nums[i]] = i;
+                }
+            }
+            //if no match found return empty array
+            return new int[] { };
         }
     }
 }

c7f1611e3deef488d6fe66177eefac2ffdc25b35
nafizrifat <imtiaz_rifat@yahoo.com>
2026-05-21 06:42:56 -0500

    383 RansomNote

---
+++ LeetCode.Test/0331-600/_383_RansomNote_Test.cs
@@ -0,0 +1,61 @@
+﻿using LeetCode._0331_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._0331_600
+{
+    [TestClass]
+    public class _383_RansomNote_Test
+    {
+        [TestMethod]
+        public void CanConstruct_WhenMagazineDoesNotContainCharacter_ReturnsFalse()
+        {
+            // Arrange
+            _383_RansomNote solution = new _383_RansomNote();
+
+            string ransomNote = "a";
+            string magazine = "b";
+
+            // Act
+            bool result = solution.CanConstruct(ransomNote, magazine);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CanConstruct_WhenMagazineDoesNotHaveEnoughCharacters_ReturnsFalse()
+        {
+            // Arrange
+            _383_RansomNote solution = new _383_RansomNote();
+
+            string ransomNote = "aa";
+            string magazine = "ab";
+
+            // Act
+            bool result = solution.CanConstruct(ransomNote, magazine);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CanConstruct_WhenMagazineHasEnoughCharacters_ReturnsTrue()
+        {
+            // Arrange
+            _383_RansomNote solution = new _383_RansomNote();
+
+            string ransomNote = "aa";
+            string magazine = "aab";
+
+            // Act
+            bool result = solution.CanConstruct(ransomNote, magazine);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ LeetCode/0331-0600/383-RansomNote.cs
@@ -0,0 +1,47 @@
+﻿//-----------------------------------------------------------------------------
+// Link: https://leetcode.com/problems/ransom-note/
+//-----------------------------------------------------------------------------
+
+namespace LeetCode._0331_0600
+{
+    public class _383_RansomNote
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            if (ransomNote.Length > magazine.Length)
+                return false;
+
+            Dictionary<char, int> dicRansomNote = new Dictionary<char, int>();
+
+            foreach (char mc in magazine)
+            {
+                if (dicRansomNote.ContainsKey(mc))
+                {
+                    dicRansomNote[mc]++;
+                }
+                else
+                {
+                    dicRansomNote.Add(mc, 1);
+                }
+            }
+
+            foreach (char c in ransomNote)
+            {
+                if (dicRansomNote.ContainsKey(c))
+                {
+                    if (dicRansomNote[c] == 1)
+                    {
+                        dicRansomNote.Remove(c);
+                    }
+                    else
+                    {
+                        dicRansomNote[c]--;
+                    }
+                }
+                else { return false; }
+            }
+
+            return true;
+        }
+    }
+}

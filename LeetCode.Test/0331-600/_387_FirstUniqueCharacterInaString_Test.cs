83ba94f4385090fb04b2b73034ba1a77bbe1e4bb
nafizrifat <imtiaz_rifat@yahoo.com>
2026-05-22 06:52:40 -0500

    Problem 387 dic approch and alternate Character Frequency Array approch

---
+++ LeetCode.Test/0331-600/_387_FirstUniqueCharacterInaString_Test.cs
@@ -0,0 +1,92 @@
+﻿using LeetCode._0331_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._0331_600
+{
+    [TestClass]
+    public class _387_FirstUniqueCharacterInaString_Test
+    {
+        [TestMethod]
+        public void FirstUniqChar_WithLeetCode_Returns0()
+        {
+            // Arrange
+            var solution = new _387_FirstUniqueCharacterInaString();
+
+            // Act
+            int result = solution.FirstUniqChar("leetcode");
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void FirstUniqChar_WithLoveLeetCode_Returns2()
+        {
+            // Arrange
+            var solution = new _387_FirstUniqueCharacterInaString();
+
+            // Act
+            int result = solution.FirstUniqChar("loveleetcode");
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void FirstUniqChar_WithAllRepeatingCharacters_ReturnsMinus1()
+        {
+            // Arrange
+            var solution = new _387_FirstUniqueCharacterInaString();
+
+            // Act
+            int result = solution.FirstUniqChar("aabb");
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void FirstUniqChar_WithSingleCharacter_Returns0()
+        {
+            // Arrange
+            var solution = new _387_FirstUniqueCharacterInaString();
+
+            // Act
+            int result = solution.FirstUniqChar("z");
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void FirstUniqChar_WithUniqueCharacterAtEnd_ReturnsLastIndex()
+        {
+            // Arrange
+            var solution = new _387_FirstUniqueCharacterInaString();
+
+            // Act
+            int result = solution.FirstUniqChar("aabbc");
+
+            // Assert
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void FirstUniqChar_WithEmptyString_ReturnsMinus1()
+        {
+            // Arrange
+            var solution = new _387_FirstUniqueCharacterInaString();
+
+            // Act
+            int result = solution.FirstUniqChar("");
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+
+    }
+}
---
+++ LeetCode/0331-0600/387-FirstUniqueCharacterInaString.cs
@@ -0,0 +1,72 @@
+﻿//-----------------------------------------------------------------------------
+// Link: https://leetcode.com/problems/first-unique-character-in-a-string/
+//-----------------------------------------------------------------------------
+namespace LeetCode._0331_0600
+{
+    public class _387_FirstUniqueCharacterInaString
+    {
+        public int FirstUniqChar(string s)
+        {
+
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+
+            foreach (char c in s)
+            {
+                if (dic.ContainsKey(c))
+                {
+                    dic[c]++;
+                }
+                else
+                {
+                    dic.Add(c, 1);
+                }
+            }
+
+            //foreach (char c in s)
+            //{
+            //    if (dic.ContainsKey(c) && (dic[c] == 1))
+            //    {
+            //        return s.IndexOf(c);
+            //    }
+            //}
+
+
+            //Because IndexOf() scans the string again from the beginning.
+            //So although your solution looks like O(n),
+            //technically it can become worse due to repeated searches.
+            // Find first unique char
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dic[s[i]] == 1)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+
+            //Optimization using ARRAY
+            //int[] freq = new int[26];
+
+            //foreach (char c in s)
+            //{
+            //    //Using character ASCII/Unicode values to convert letters into array indexes.
+            //    //if c='b'
+            //    //ASCII valus of b = 98 & a = 97
+            //    // 'b' - 'a' => 98-97 => 1
+            //    // so index 1 of the Array
+            //    freq[c - 'a']++;
+            //}
+
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            //    if (freq[s[i] - 'a'] == 1)
+            //    {
+            //        return i;
+            //    }
+            //}
+
+            //return -1;
+        }
+    }
+}

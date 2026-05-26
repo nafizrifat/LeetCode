48dd6f0e7510e2643392b5b573b89cd87f2f2336
nafizrifat <imtiaz_rifat@yahoo.com>
2026-05-26 07:52:21 -0500

    169 MajorityElement

---
+++ LeetCode.Test/0001-0300/_169_MajorityElement_Test.cs
@@ -0,0 +1,117 @@
+﻿using LeetCode._0001_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._0001_0300
+{
+    [TestClass]
+    public class _169_MajorityElement_Test
+    {
+        [TestMethod]
+        public void TestMajorityElement_NormalCase1()
+        {
+            // Arrange
+            int[] nums = { 3, 2, 3 };
+
+            // Act
+            int result = new _169_MajorityElement().MajorityElement(nums);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void TestMajorityElement_NormalCase2()
+        {
+            // Arrange
+            int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
+
+            // Act
+            int result = new _169_MajorityElement().MajorityElement(nums);
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestMajorityElement_SingleElement()
+        {
+            // Arrange
+            int[] nums = { 1 };
+
+            // Act
+            int result = new _169_MajorityElement().MajorityElement(nums);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMajorityElement_MajorityAtBeginning()
+        {
+            // Arrange
+            int[] nums = { 5, 5, 5, 2, 3 };
+
+            // Act
+            int result = new _169_MajorityElement().MajorityElement(nums);
+
+            // Assert
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void TestMajorityElement_MajorityAtEnd()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 4, 4, 4, 4 };
+
+            // Act
+            int result = new _169_MajorityElement().MajorityElement(nums);
+
+            // Assert
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void TestMajorityElement_NegativeNumbers()
+        {
+            // Arrange
+            int[] nums = { -1, -1, -1, 2, 3 };
+
+            // Act
+            int result = new _169_MajorityElement().MajorityElement(nums);
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestMajorityElement_AllSameNumbers()
+        {
+            // Arrange
+            int[] nums = { 7, 7, 7, 7, 7 };
+
+            // Act
+            int result = new _169_MajorityElement().MajorityElement(nums);
+
+            // Assert
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void TestMajorityElement_LargeValues()
+        {
+            // Arrange
+            int[] nums = { 999999, 1, 999999, 999999 };
+
+            // Act
+            int result = new _169_MajorityElement().MajorityElement(nums);
+
+            // Assert
+            Assert.AreEqual(999999, result);
+        }
+    }
+}
---
+++ LeetCode/0001-0300/169-MajorityElement.cs
@@ -0,0 +1,55 @@
+﻿//-----------------------------------------------------------------------------
+// Link: https://leetcode.com/problems/valid-anagram/description/
+//-----------------------------------------------------------------------------
+
+namespace LeetCode._0001_0300
+{
+    public class _169_MajorityElement
+    {
+        public int MajorityElement(int[] nums)
+        {
+            //int ret= -1;
+            //int majorityCount = nums.Length / 2;
+
+            //Dictionary<int, int> dNums = new Dictionary<int, int>();
+
+            //foreach (int x in nums)
+            //{
+            //    if (dNums.ContainsKey(x))
+            //    {
+            //        dNums[x]++;
+            //    }
+            //    else
+            //    {
+            //        dNums.Add(x, 1);
+            //    }
+
+            //    if (dNums[x] > majorityCount)
+            //        ret = x;
+            //}
+
+            //return ret;
+
+            int majorityCount = nums.Length / 2;
+
+            Dictionary<int, int> numCounts = new Dictionary<int, int>();
+
+            foreach (int x in nums)
+            {
+                if (numCounts.ContainsKey(x))
+                {
+                    numCounts[x]++;
+                }
+                else
+                {
+                    numCounts.Add(x, 1);
+                }
+
+                if (numCounts[x] > majorityCount)
+                    return x;
+            }
+
+            return -1;
+        }
+    }
+}

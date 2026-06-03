3aa74acdeb4b4daab7e7c56068b55560f4767c1b
nafizrifat <imtiaz_rifat@yahoo.com>
2026-06-03 07:27:42 -0500

    347 TopKFrequentElements + Bucket Sort

---
+++ LeetCode.Test/0301-600/_347_TopKFrequentElements_Test.cs
@@ -0,0 +1,127 @@
+﻿using LeetCode._0301_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._0301_600
+{
+    [TestClass]
+    public class _347_TopKFrequentElements_Test
+    {
+        private _347_TopKFrequentElements _solution;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _solution = new _347_TopKFrequentElements();
+        }
+
+        [TestMethod]
+        public void TopKFrequent_Example1_ReturnsTopTwoFrequentElements()
+        {
+            // Arrange
+            int[] nums = { 1, 1, 1, 2, 2, 3 };
+            int k = 2;
+
+            // Act
+            int[] result = _solution.TopKFrequent(nums, k);
+
+            // Assert
+            CollectionAssert.AreEquivalent(new int[] { 1, 2 }, result);
+        }
+
+        [TestMethod]
+        public void TopKFrequent_Example2_ReturnsSingleElement()
+        {
+            // Arrange
+            int[] nums = { 1 };
+            int k = 1;
+
+            // Act
+            int[] result = _solution.TopKFrequent(nums, k);
+
+            // Assert
+            CollectionAssert.AreEquivalent(new int[] { 1 }, result);
+        }
+
+        [TestMethod]
+        public void TopKFrequent_WithNegativeNumbers_ReturnsMostFrequentElements()
+        {
+            // Arrange
+            int[] nums = { -1, -1, -1, 2, 2, 3 };
+            int k = 2;
+
+            // Act
+            int[] result = _solution.TopKFrequent(nums, k);
+
+            // Assert
+            CollectionAssert.AreEquivalent(new int[] { -1, 2 }, result);
+        }
+
+        [TestMethod]
+        public void TopKFrequent_WhenKEqualsNumberOfUniqueElements_ReturnsAllUniqueElements()
+        {
+            // Arrange
+            int[] nums = { 4, 1, 2, 2, 3, 3 };
+            int k = 4;
+
+            // Act
+            int[] result = _solution.TopKFrequent(nums, k);
+
+            // Assert
+            CollectionAssert.AreEquivalent(new int[] { 1, 2, 3, 4 }, result);
+        }
+
+        [TestMethod]
+        public void TopKFrequent_WithSameFrequency_ReturnsAnyValidTopKElements()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 4 };
+            int k = 2;
+
+            // Act
+            int[] result = _solution.TopKFrequent(nums, k);
+
+            // Assert
+            Assert.AreEqual(k, result.Length);
+            Assert.IsTrue(result.All(x => nums.Contains(x)));
+            Assert.AreEqual(result.Distinct().Count(), result.Length);
+        }
+
+        [TestMethod]
+        public void TopKFrequent_WithLargeFrequencyDifference_ReturnsCorrectElement()
+        {
+            // Arrange
+            int[] nums = { 5, 5, 5, 5, 1, 1, 2, 3 };
+            int k = 1;
+
+            // Act
+            int[] result = _solution.TopKFrequent(nums, k);
+
+            // Assert
+            CollectionAssert.AreEquivalent(new int[] { 5 }, result);
+        }
+
+        [TestMethod]
+        public void TopKFrequent_WithMultipleTopElements_ReturnsCorrectTopK()
+        {
+            // Arrange
+            int[] nums = { 1, 1, 2, 2, 2, 3, 3, 4 };
+            int k = 3;
+
+            // Frequencies:
+            // 2 -> 3 times
+            // 1 -> 2 times
+            // 3 -> 2 times
+            // 4 -> 1 time
+
+            // Act
+            int[] result = _solution.TopKFrequent(nums, k);
+
+            // Assert
+            CollectionAssert.AreEquivalent(new int[] { 1, 2, 3 }, result);
+        }
+    }
+}
---
+++ LeetCode/0301-0600/347-TopKFrequentElements.cs
@@ -0,0 +1,93 @@
+﻿//-----------------------------------------------------------------------------
+// Link: https://leetcode.com/problems/top-k-frequent-elements/
+// Algo Used: Dictionary + Bucket Sort
+//
+// Main Concept:
+// Count each number's frequency, then place numbers into buckets where
+// bucket index = frequency.
+//
+// Approach:
+// 1. Use Dictionary<int, int> to count frequency of each number.
+// 2. Create buckets of size nums.Length + 1 because max frequency can be nums.Length.
+// 3. Put each number into buckets[count].
+// 4. Traverse buckets from high frequency to low frequency.
+// 5. Add numbers to result until result.Count == k.
+//
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+//-----------------------------------------------------------------------------
+
+using System.Xml.Linq;
+
+namespace LeetCode._0301_0600
+{
+    public class _347_TopKFrequentElements
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+
+            foreach (int num in nums)
+            {
+                if (freq.ContainsKey(num))
+                {
+                    freq[num]++;
+                }
+                else
+                {
+                    freq.Add(num, 1);
+                }
+            }
+            //// Sorts descending, takes the first k elements, and extracts only the keys
+            //int[] topKVlues = freq
+            //    .OrderByDescending(pair => pair.Value)
+            //    .Select(pair => pair.Key)
+            //    .Take(k)
+            //    .ToArray();
+
+            ////O(n log n)
+            ////where m is the number of unique elements.
+            ////In the worst case, if all numbers are unique, m = n, so time complexity becomes:
+
+            //return topKVlues;
+
+            List<int>[] buckets = new List<int>[nums.Length + 1];
+
+            foreach (KeyValuePair<int, int> pair in freq)
+            {
+                int number = pair.Key;
+                int count = pair.Value;
+
+                if (buckets[count] == null)
+                {
+                    buckets[count] = new List<int>();
+                }
+
+                buckets[count].Add(number);
+            }
+
+            List<int> result = new List<int>();
+
+            for (int i = buckets.Length - 1; i >= 0; i--)
+            {
+                if (buckets[i] != null)
+                {
+                    foreach (int number in buckets[i])
+                    {
+                        result.Add(number);
+
+                        if (result.Count == k)
+                        {
+                            return result.ToArray();
+                        }
+                    }
+                }
+            }
+
+            return result.ToArray();
+
+
+
+        }
+    }
+}

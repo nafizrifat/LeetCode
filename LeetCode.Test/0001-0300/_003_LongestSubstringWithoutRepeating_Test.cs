a3169ec53f5c91517f8bb4e0539ce70330593a03
nafizrifat <imtiaz_rifat@yahoo.com>
2026-06-05 07:55:02 -0500

    003_LongestSubstringWithoutRepeating + Sliding Window

---
+++ LeetCode.Test/0001-0300/_003_LongestSubstringWithoutRepeating_Test.cs
@@ -0,0 +1,153 @@
+﻿using LeetCode._0001_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._0001_0300
+{
+    [TestClass]
+    public class _003_LongestSubstringWithoutRepeating_Test
+    {
+        [TestMethod]
+        public void LengthOfLongestSubstring_Example1_Returns3()
+        {
+            // Arrange
+            var solution = new _003_LongestSubstringWithoutRepeating();
+            string s = "abcabcbb";
+
+            // Act
+            int result = solution.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void LengthOfLongestSubstring_Example2_Returns1()
+        {
+            // Arrange
+            var solution = new _003_LongestSubstringWithoutRepeating();
+            string s = "bbbbb";
+
+            // Act
+            int result = solution.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void LengthOfLongestSubstring_Example3_Returns3()
+        {
+            // Arrange
+            var solution = new _003_LongestSubstringWithoutRepeating();
+            string s = "pwwkew";
+
+            // Act
+            int result = solution.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void LengthOfLongestSubstring_EmptyString_Returns0()
+        {
+            // Arrange
+            var solution = new _003_LongestSubstringWithoutRepeating();
+            string s = "";
+
+            // Act
+            int result = solution.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void LengthOfLongestSubstring_SingleCharacter_Returns1()
+        {
+            // Arrange
+            var solution = new _003_LongestSubstringWithoutRepeating();
+            string s = "a";
+
+            // Act
+            int result = solution.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void LengthOfLongestSubstring_AllUniqueCharacters_ReturnsLength()
+        {
+            // Arrange
+            var solution = new _003_LongestSubstringWithoutRepeating();
+            string s = "abcdef";
+
+            // Act
+            int result = solution.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void LengthOfLongestSubstring_RepeatedCharactersAfterUniquePart_Returns3()
+        {
+            // Arrange
+            var solution = new _003_LongestSubstringWithoutRepeating();
+            string s = "dvdf";
+
+            // Act
+            int result = solution.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void LengthOfLongestSubstring_RepeatingCharacterInMiddle_Returns2()
+        {
+            // Arrange
+            var solution = new _003_LongestSubstringWithoutRepeating();
+            string s = "abba";
+
+            // Act
+            int result = solution.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void LengthOfLongestSubstring_StringWithSpaces_Returns3()
+        {
+            // Arrange
+            var solution = new _003_LongestSubstringWithoutRepeating();
+            string s = "a b a";
+
+            // Act
+            int result = solution.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void LengthOfLongestSubstring_LongInputWithRepeats_Returns5()
+        {
+            // Arrange
+            var solution = new _003_LongestSubstringWithoutRepeating();
+            string s = "abcdeafgh";
+
+            // Act
+            int result = solution.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.AreEqual(8, result);
+        }
+    }
+}
---
+++ LeetCode/0001-0300/003-LongestSubstringWithoutRepeating.cs
@@ -0,0 +1,55 @@
+﻿//-----------------------------------------------------------------------------
+// Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+//-----------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._0001_0300
+{
+    public class _003_LongestSubstringWithoutRepeating
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            //// Convert to char array
+            //char[] characters = s.ToCharArray();
+
+            //HashSet<char> seen = new HashSet<char>();
+            //int maxlength = 0;
+
+            //for (int i = 0; i < characters.Length; i++) {
+
+            //    if (!seen.Add(characters[i])) {
+            //        int currentMaxlength = seen.Count;
+            //        if(currentMaxlength> maxlength)
+            //            maxlength = currentMaxlength;
+            //        seen = new HashSet<char>();
+            //    }            
+            //}
+
+            //return maxlength;
+
+
+            HashSet<char> set = new HashSet<char>();
+
+            int left = 0;
+            int maxLength = 0;
+
+            for (int right = 0; right < s.Length; right++)
+            {
+                char current = s[right];
+                while (set.Contains(current))
+                {
+                    set.Remove(s[left]);
+                    left++;
+                }
+
+                set.Add(current);
+                int currentLength = right - left + 1;
+                maxLength = Math.Max(maxLength, currentLength);
+            }
+
+            return maxLength;
+        }
+    }
+}

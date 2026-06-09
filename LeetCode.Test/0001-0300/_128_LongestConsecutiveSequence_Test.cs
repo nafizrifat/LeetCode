235e2ff48c627a6f309a4d184abc9e91a2d4ae34
nafizrifat <imtiaz_rifat@yahoo.com>
2026-06-08 21:56:43 -0500

    128 LongestConsecutiveSequence

---
+++ LeetCode.Test/0001-0300/_128_LongestConsecutiveSequence_Test.cs
@@ -0,0 +1,169 @@
+﻿using LeetCode._0001_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._0001_0300
+{
+    [TestClass]
+    public class _128_LongestConsecutiveSequence_Test
+    {
+        [TestMethod]
+        public void LongestConsecutive_Example1_Returns4()
+        {
+            // Arrange
+            var solution = new _128_LongestConsecutiveSequence();
+            int[] nums = { 100, 4, 200, 1, 3, 2 };
+
+            // Act
+            int result = solution.LongestConsecutive(nums);
+
+            // Assert
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void LongestConsecutive_Example2_Returns9()
+        {
+            // Arrange
+            var solution = new _128_LongestConsecutiveSequence();
+            int[] nums = { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 };
+
+            // Act
+            int result = solution.LongestConsecutive(nums);
+
+            // Assert
+            Assert.AreEqual(9, result);
+        }
+
+        [TestMethod]
+        public void LongestConsecutive_WithDuplicates_Returns3()
+        {
+            // Arrange
+            var solution = new _128_LongestConsecutiveSequence();
+            int[] nums = { 1, 0, 1, 2 };
+
+            // Act
+            int result = solution.LongestConsecutive(nums);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void LongestConsecutive_EmptyArray_Returns0()
+        {
+            // Arrange
+            var solution = new _128_LongestConsecutiveSequence();
+            int[] nums = { };
+
+            // Act
+            int result = solution.LongestConsecutive(nums);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void LongestConsecutive_SingleNumber_Returns1()
+        {
+            // Arrange
+            var solution = new _128_LongestConsecutiveSequence();
+            int[] nums = { 5 };
+
+            // Act
+            int result = solution.LongestConsecutive(nums);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void LongestConsecutive_NegativeNumbers_Returns4()
+        {
+            // Arrange
+            var solution = new _128_LongestConsecutiveSequence();
+            int[] nums = { -1, -2, -3, -4, 10 };
+
+            // Act
+            int result = solution.LongestConsecutive(nums);
+
+            // Assert
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void LongestConsecutive_UnorderedNegativeAndPositiveNumbers_Returns7()
+        {
+            // Arrange
+            var solution = new _128_LongestConsecutiveSequence();
+            int[] nums = { -2, -1, 0, 1, 2, 3, 4, 10 };
+
+            // Act
+            int result = solution.LongestConsecutive(nums);
+
+            // Assert
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void LongestConsecutive_NoConsecutiveNumbers_Returns1()
+        {
+            // Arrange
+            var solution = new _128_LongestConsecutiveSequence();
+            int[] nums = { 10, 30, 50, 70 };
+
+            // Act
+            int result = solution.LongestConsecutive(nums);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void LongestConsecutive_AllSameNumbers_Returns1()
+        {
+            // Arrange
+            var solution = new _128_LongestConsecutiveSequence();
+            int[] nums = { 2, 2, 2, 2, 2 };
+
+            // Act
+            int result = solution.LongestConsecutive(nums);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void LongestConsecutive_MultipleSequences_Returns5()
+        {
+            // Arrange
+            var solution = new _128_LongestConsecutiveSequence();
+            int[] nums = { 10, 5, 12, 3, 55, 30, 4, 11, 2, 1 };
+
+            // Act
+            int result = solution.LongestConsecutive(nums);
+
+            // Assert
+            Assert.AreEqual(5, result);
+        }
+        [TestMethod]
+        public void LongestConsecutive_LaterSequenceIsLongest_Returns4()
+        {
+            // Arrange
+            var solution = new _128_LongestConsecutiveSequence();
+            int[] nums = { 10, 20, 30, 1, 2, 100, 101, 102, 103 };
+
+            // Act
+            int result = solution.LongestConsecutive(nums);
+
+            // Assert
+            Assert.AreEqual(4, result);
+        }
+
+    }
+}
+
+
---
+++ LeetCode/0001-0300/128-LongestConsecutiveSequence.cs
@@ -0,0 +1,66 @@
+﻿//-----------------------------------------------------------------------------
+// Link: https://leetcode.com/problems/longest-consecutive-sequence/description/
+//-----------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace LeetCode._0001_0300
+{
+    public class _128_LongestConsecutiveSequence
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            //if(nums == null || nums.Length == 0)
+            //    return 0;
+
+            //int longest = 1;
+            //int currentLength = 1;
+
+            ////HashSet<int> map = new HashSet<int>(nums);
+            //int[] numsArray = nums.OrderBy(x => x).ToArray(); ;
+
+            //for (int i = 0; i < numsArray.Length; i++) {
+            //    //if (i + 1 >= numsArray.Length)
+            //    //    return ret;
+
+            //    if (numsArray[i] == numsArray[i + 1])
+            //        continue;
+
+            //    if (numsArray[i+1] == numsArray[i] + 1)
+            //        currentLength++;
+            //    else
+            //        currentLength = 1;
+
+            //    longest = Math.Max(longest, currentLength);
+
+
+            //}
+            //return longest;
+
+            if (nums == null || nums.Length == 0)
+                return 0;
+
+            HashSet<int> set = new HashSet<int>(nums);
+
+            int longest = 0;
+
+            foreach (int num in set) { 
+
+                if(!set.Contains(num-1))
+                {
+                    int currentNum = num;
+                    int currentLength = 1;
+
+                    while (set.Contains(currentNum + 1))
+                    {
+                        currentLength++;
+                        currentNum++;
+                    }
+                    longest = Math.Max(longest, currentLength);
+                }
+
+            }
+            return longest;
+        }
+    }
+}

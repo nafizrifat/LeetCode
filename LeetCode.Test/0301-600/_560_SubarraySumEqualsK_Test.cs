53701ddcff48ec954bcb7ea347419c8c1d058db5
nafizrifat <imtiaz_rifat@yahoo.com>
2026-08-05 13:14:15 -0500

    560 SubarraySumEqualsK

---
+++ LeetCode.Test/0301-600/_560_SubarraySumEqualsK_Test.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode._0301_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test._0301_600
+{
+    [TestClass]
+    public class _560_SubarraySumEqualsK_Test
+    {
+        [DataTestMethod]
+        [DataRow(new int[] { 1, 1, 1 }, 2, 2)]
+        [DataRow(new int[] { 1, 2, 3 }, 3, 2)]
+        [DataRow(new int[] { 1 }, 1, 1)]
+        [DataRow(new int[] { 1 }, 0, 0)]
+        [DataRow(new int[] { -1, -1, 1 }, 0, 1)]
+        [DataRow(new int[] { 1, -1, 0 }, 0, 3)]
+        [DataRow(new int[] { 0, 0, 0 }, 0, 6)]
+        [DataRow(new int[] { 3, 4, 7, 2, -3, 1, 4, 2 }, 7, 4)]
+        [DataRow(new int[] { -1, -1, -1 }, -2, 2)]
+        [DataRow(new int[] { 5, -2, 3 }, 6, 1)]
+        public void SubarraySum_ReturnsExpectedCount(
+            int[] nums,
+            int k,
+            int expected)
+        {
+            // Arrange
+            var solution = new _560_SubarraySumEqualsK();
+
+            // Act
+            int actual = solution.SubarraySum(nums, k);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ LeetCode/0301-0600/560-SubarraySumEqualsK.cs
@@ -0,0 +1,73 @@
+﻿//-----------------------------------------------------------------------------
+// Link: https://leetcode.com/problems/subarray-sum-equals-k/description/
+//-----------------------------------------------------------------------------
+
+namespace LeetCode._0301_0600
+{
+    public class _560_SubarraySumEqualsK
+    {
+        public int SubarraySum(int[] nums, int k)
+        {
+            //int prefixSum = 0;
+            //int count = 0;
+            //int[] remainderCount = new int[k];
+            //int reminder = 0;
+            //// Empty prefix sum has remainder 0
+            ////remainderCount[0] = 1;
+
+            //foreach (int num in nums)
+            //{
+            //    prefixSum += num;
+
+            //    if (prefixSum == k)
+            //        count++;
+            //    else
+            //    {
+            //        reminder = prefixSum % k;
+
+            //        count += remainderCount[reminder];
+            //        remainderCount[reminder]++;
+            //    }
+            //}
+
+            //return count;
+            int prefixSum = 0;
+            int count = 0;
+
+            Dictionary<int, int> prefixSumCount = new()
+            {
+                [0] = 1
+            };
+
+            foreach (int num in nums)
+            {
+                prefixSum += num;
+
+                //if (prefixSumCount.TryGetValue(prefixSum - k, out int frequency))
+                //{
+                //    count += frequency;
+                //}
+                int requiredPrefixSum = prefixSum - k;
+
+                if (prefixSumCount.ContainsKey(requiredPrefixSum))
+                {
+                    int frequency = prefixSumCount[requiredPrefixSum];
+                    count += frequency;
+                }
+
+                //prefixSumCount[prefixSum] =
+                //    prefixSumCount.GetValueOrDefault(prefixSum) + 1;
+                if (prefixSumCount.ContainsKey(prefixSum))
+                {
+                    prefixSumCount[prefixSum]++;
+                }
+                else
+                {
+                    prefixSumCount[prefixSum] = 1;
+                }
+            }
+
+            return count;
+        }
+    }
+}
